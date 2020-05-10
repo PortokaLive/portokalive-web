c6eafcdb52bf16c2aea262d5e3ba1d33ab14e613
m3yevn <mmm.myintmyat@gmail.com>
2020-05-10 15:11:22 +0800

    ⚓️ add: environment based axios client

--- src/utils/actions/Axios.ts
+++ src/utils/actions/Axios.ts
@@ -5,8 +5,29 @@ export default () => {
     axios.defaults.headers.common["Authorization"] = localStorage.getItem(
       "token"
     );
+
+  let backendUrl;
+  let backendVersion;
+  const {
+    REACT_APP_ENVIRONMENT: env,
+    REACT_APP_BACKEND_URL_DEV: devUrl,
+    REACT_APP_BACKEND_URL_PROD: prodUrl,
+    REACT_APP_BACKEND_VERSION_DEV: devVer,
+    REACT_APP_BACKEND_VERSION_PROD: prodVer,
+  } = process.env;
+  switch (env) {
+    case "PROD":
+      backendUrl = prodUrl;
+      backendVersion = prodVer;
+      break;
+    case "DEV":
+      backendUrl = devUrl;
+      backendVersion = devVer;
+      break;
+  }
+
   const client = axios.create({
-    baseURL: `${process.env.REACT_APP_BACKEND_URL}/${process.env.REACT_APP_BACKEND_VERSION}`,
+    baseURL: `${backendUrl}/${backendVersion}`,
   });
   return client;
 };
