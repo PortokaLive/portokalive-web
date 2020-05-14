ff57308fe93b0ef6b10cef28c136e3dcfab038d3
m3yevn <mmm.myintmyat@gmail.com>
2020-05-15 01:21:32 +0800

    🏆 implement: server sent events

--- src/pages/app/LiveStreamGrid.tsx
+++ src/pages/app/LiveStreamGrid.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const LiveStreamGrid = () => {
-  const streamers = [
-    { name: "", thumbnail: "" },
-    { name: "", thumbnail: "" },
-    { name: "", thumbnail: "" },
-    { name: "", thumbnail: "" },
-  ];
+  const [streamers, setStreamers] = useState<any[]>([]);
+  const [listening, setListening] = useState(false);
+  const updateStreamers = (streams: any[]) => {
+    setStreamers([...streams]);
+  };
+
+  useEffect(() => {
+    if (!listening) {
+      const events = new EventSource(
+        "http://localhost:8000/sse/streams?sseKey=4Jp2Q3RyIu"
+      );
+
+      events.onmessage = (event) => {
+        console.log(JSON.parse(event.data));
+        updateStreamers(JSON.parse(event.data));
+      };
+    }
+    setListening(true);
+  }, [listening, streamers]);
+
   return (
     <>
       <div className="row no-gutters mt-5 pt-5 overflow-hidden">
