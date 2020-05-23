1c0ca52f2bc2b603783008dcd8e0b7a18a104ee7
m3yevn <mmm.myintmyat@gmail.com>
2020-05-24 03:06:36 +0800

    add: https flv streaming

--- src/pages/app/LiveStreamView.tsx
+++ src/pages/app/LiveStreamView.tsx
@@ -5,100 +5,104 @@ import flv from "flv.js";
 export const LiveStreamView = ({ history, match }: any) => {
   const videoRef_A = useRef<any>(null);
   const videoRef_B = useRef<any>(null);
-  const [load, setLoad] = useState("A");
-  const [show, setShow] = useState("");
+  const [load_A, setLoad_A] = useState(true);
+  const [load_B, setLoad_B] = useState(false);
+  const [show, setShow] = useState("A");
   const { streamerId } = match.params;
 
   useEffect(() => {
-    switch (load) {
-      case "A":
-        const player_A = flv.createPlayer({
-          type: "flv",
-          url: `${getAPI()}/stream/${streamerId}.flv?token=${getToken()}`,
-        });
-        player_A.on("error", (err) => {
-          history.push("/app");
-        });
-        player_A.attachMediaElement(videoRef_A.current);
-        player_A.load();
+    if (load_A) {
+      const player_A = flv.createPlayer({
+        type: "flv",
+        url: `${getAPI()}/stream/${streamerId}.flv?token=${getToken()}`,
+      });
+      player_A.attachMediaElement(videoRef_A.current);
+      player_A.load();
+      player_A.on("error", () => {
+        history.push("/");
+      });
 
-        const video_A: HTMLVideoElement = videoRef_A.current;
-        video_A.onplaying = () => {
-          setImmediate(() => {
-            setShow("A");
-          });
-          let timeCounter = 0;
-          const timer = setInterval(() => {
-            if (timeCounter >= 10 && timeCounter >= video_A.currentTime - 2) {
-              setLoad("B");
-              clearInterval(timer);
-            }
-            timeCounter++;
-          }, 1000);
-        };
-        return () => {
-          player_A.unload();
-        };
-      case "B":
-        const player_B = flv.createPlayer({
-          type: "flv",
-          url: `${getAPI()}/stream/${streamerId}.flv?token=${getToken()}`,
+      const video_A: HTMLVideoElement = videoRef_A.current;
+      video_A.onplaying = () => {
+        setImmediate(() => {
+          setShow("A");
         });
-        player_B.on("error", (err) => {
-          history.push("/app");
-        });
-        player_B.attachMediaElement(videoRef_A.current);
-        player_B.load();
-        player_B.attachMediaElement(videoRef_B.current);
-        player_B.load();
+        let timeCounter = 0;
+        const timer = setInterval(() => {
+          if (timeCounter >= 10 && timeCounter >= video_A.currentTime - 2) {
+            setLoad_B(true);
+            setLoad_A(false);
+            clearInterval(timer);
+          }
+          timeCounter++;
+        }, 1000);
+      };
 
-        const video_B: HTMLVideoElement = videoRef_B.current;
-        video_B.onplaying = () => {
-          setImmediate(() => {
-            setShow("B");
-          });
-          let timeCounter = 0;
-          const timer = setInterval(() => {
-            if (timeCounter >= 10 && timeCounter >= video_B.currentTime - 2) {
-              setLoad("A");
-              clearInterval(timer);
-            }
-            timeCounter++;
-          }, 1000);
-        };
-        return () => {
-          player_B.unload();
-        };
-      default:
-        break;
+      return () => {
+        player_A.unload();
+      };
     }
-  }, [streamerId, load, history]);
+  }, [streamerId, load_A, history]);
+
+  useEffect(() => {
+    if (load_B) {
+      const player_B = flv.createPlayer({
+        type: "flv",
+        url: `${getAPI()}/stream/${streamerId}.flv?token=${getToken()}`,
+      });
+      player_B.attachMediaElement(videoRef_B.current);
+      player_B.load();
+      player_B.on("error", () => {
+        history.push("/");
+      });
+
+      const video_B: HTMLVideoElement = videoRef_B.current;
+      video_B.onplaying = () => {
+        setImmediate(() => {
+          setShow("B");
+        });
+        let timeCounter = 0;
+        const timer = setInterval(() => {
+          if (timeCounter >= 10 && timeCounter >= video_B.currentTime - 2) {
+            setLoad_A(true);
+            setLoad_B(false);
+            clearInterval(timer);
+          }
+          timeCounter++;
+        }, 1000);
+      };
+
+      return () => {
+        player_B.unload();
+      };
+    }
+  }, [streamerId, load_B, history]);
 
   return (
     <>
       {
         <video
-          hidden={show != "A"}
+          hidden={show !== "A"}
           ref={videoRef_A}
           style={{
             width: "100vw",
             height: "100vh",
             background: "black",
           }}
-          controls={true}
+          controls={false}
           autoPlay={true}
         />
       }
       {
         <video
-          hidden={show != "B"}
+          hidden={show !== "B"}
           ref={videoRef_B}
           style={{
             width: "100vw",
             height: "100vh",
             background: "black",
           }}
-          controls={true}
+          controls={false}
           autoPlay={true}
         />
       }
